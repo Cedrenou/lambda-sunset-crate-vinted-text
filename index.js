3e7915c7cf451ea1c44e0289271b36c10c96504b
cedricrenouleau <ced241@gmail.com>
2025-06-25 17:06:10 +0200

    chore: nettoyage du package.json pour lambda minimaliste

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
 exports.handler = async (event) => {
     console.log('Event:', JSON.stringify(event, null, 2));
-    
-    const response = {
-        statusCode: 200,
-        headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Headers': 'Content-Type',
-            'Access-Control-Allow-Methods': 'GET, POST, OPTIONS'
-        },
-        body: JSON.stringify({
-            message: 'Hello World from Lambda!',
-            timestamp: new Date().toISOString(),
-            event: event
-        })
-    };
-    
-    return response;
 }; 
